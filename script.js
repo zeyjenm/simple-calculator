db27715fac5c0bb352e2aca90cecc6471737c727
Zeynep <zeynepmarch@gmail.com>
2022-09-11 12:19:30 +1000

    Add calculate function that calls operation sub-functions

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 
 // Calculator functions
 
-function add (a, b) {
+function addition (a, b) {
     return a + b;
 }
 
-function subtract (a, b) {
+function subtraction (a, b) {
     return a - b;
 }
 
-function multiply (a, b) {
+function multiplication (a, b) {
     return a * b;
 }
 
-function divide (a, b) {
+function division (a, b) {
     return a / b;
+}
+
+function calculate (a, b, operation) {
+    if (operation === 'plus') {
+        return addition(a, b);
+    } 
+    else if (operation === 'minus') {
+        return subtraction(a, b);
+    } 
+    else if (operation === 'multiply') {
+        return multiplication(a, b);
+    }
+    else if (operation === 'divide') {
+        return division(a, b);
+    }
 }
