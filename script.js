a755967f55bfbe01d4f75f9ea519cf8f75390baf
Zeynep <zeynepmarch@gmail.com>
2022-09-12 13:21:43 +1000

    Display the result of calculate function on calculator screen, when called

--- script.js
+++ script.js
@@ -2,32 +2,46 @@
 // Calculator functions
 
 function addition (a, b) {
-    return a + b;
+    return result = a + b;
 }
 
 function subtraction (a, b) {
-    return a - b;
+    return result = a - b;
 }
 
 function multiplication (a, b) {
-    return a * b;
+    return result = a * b;
 }
 
 function division (a, b) {
-    return a / b;
+    return result = a / b;
 }
+
+const output = document.querySelector('#result');
 
 function calculate (a, b, operation) {
     if (operation === 'plus') {
-        return addition(a, b);
+        let calculateResult = addition(a, b);
+        output.textContent = calculateResult;
+        return calculateResult;
     } 
     else if (operation === 'minus') {
-        return subtraction(a, b);
+        let calculateResult = subtraction(a, b);
+        output.textContent = calculateResult;
+        return calculateResult;
     } 
     else if (operation === 'multiply') {
-        return multiplication(a, b);
+        let calculateResult = multiplication(a, b);
+        output.textContent = calculateResult;
+        return calculateResult;
     }
     else if (operation === 'divide') {
-        return division(a, b);
+        let calculateResult = division(a, b);
+        output.textContent = calculateResult;
+        return calculateResult;
     }
 }
+
+console.log(calculate(1, 4, 'plus'));
+
+
